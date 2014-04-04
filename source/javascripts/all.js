261aaf77401d0a1fd794bfc0ebd29f75b53726e5
Deric Mendez <dmendez@degdigital.com>
2014-04-04 14:20:18 -0500

    updated scripts and interactions to angular, added google plus api call

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,43 +1,29 @@
-$('#grid-view-select').click(function(e) {
-	e.preventDefault();
-	$('#grid-view').toggleClass('selected');
-});
-$('#grid-view-favorite').click(function(e) {
-	e.preventDefault();
-	$(this).toggleClass('active');
-});
+function GmailGridView($scope) {
 
-$('#generate').click(function(e) {
-	e.preventDefault();
-	if( $('#company-name').val().length > 0 ) {
-        $('.grid-view-desc .from').text($("#company-name").val());
-        $('#company-name-markup').text($("#company-name").val());
-    }
+    $scope.featuredImageUrl = '';
+    $scope.googlePlusImageUrl = 'images/sender-image.png';
+    $scope.selected = 'form';
 
-    if( $('#company-url').val().length > 0 ) {
-        $('#company-url-markup').text($("#company-url").val());
-        //...make api call & replace sender image...//
-    }
-    else if( $('#company-url-plus').val().length <= 0 && $('#company-name').val().length > 0) {
-        $('.google-icon img').replaceWith($("#company-name").val().slice(0,1));
-    }
+    var onGooglePlusSuccess = function(data) {
+        if ( data.error ) {
 
-    if( $('#company-url-plus').val().length > 0 ) {
-        $('#company-url-plus-markup').text($("#company-url-plus").val());
+        } else {
+            console.log(data)
+            console.log(data.displayName);
+            console.log(data.image.url);
+            $scope.googlePlusImageUrl = data.image.url;                    
+        }
+    };
+    
+    $scope.fetchGooglePlusData = function() {
+        var url = $scope.googlePlus.split('+');
+        var username = url[1];
+        var apiUrl = 'https://www.googleapis.com/plus/v1/people/+' + username + '?key=AIzaSyBXM_nPfCNIDkirN6SAXnDQ78sn7dlrN9Y';
+        
+        $.ajax({
+            url: apiUrl,
+            dataType: 'jsonp',
+            success: onGooglePlusSuccess
+        })
     }
-    if( $('#featured-image').val().length > 0 ) {
-        $('.grid-view-hero').css("backgroundImage", "url('"+$("#featured-image").val()+"')");
-        $('#featured-image-markup').text($("#featured-image").val());
-    }
-    if( $('#subject-line').val().length > 0 ) {
-        $('.grid-view-desc .subject b').text($("#subject-line").val());
-    }
-
-    $('.grid-form').hide();
-    $('.microdata').show();
-});
-$('#modify').click(function(e) {
-	e.preventDefault();
-    $('.grid-form').show();
-    $('.microdata').hide();
-});
+}
